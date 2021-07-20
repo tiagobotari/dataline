8c6199edaedd5b2e2118ddf6daca579935ff9660
Tiago <tiagobotari@gmail.com>
2021-07-20 09:54:02 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,17 +7,18 @@ from __future__ import unicode_literals
 
 from setuptools import setup, find_packages
 
+INSTALL_REQUIRES = []
 
 if __name__ == "__main__":
     setup(name='Dataline',
         version='0.0'
         , author=''
-        , author_email=''
-        , license=''
+        , author_email='tiagobotari@gmail.com'
+        , description="Simple class to aid the process of data cleaning.",
+        , license='https://github.com/tiagobotari/dataline'
+        , url="https://github.com/rcabdia/CWT"
         , packages=find_packages()
+        , install_requires=INSTALL_REQUIRES
         , zip_safe=False
-        , install_requires=[
-            ]
         , python_requires='>=3.6'
     )
-        
